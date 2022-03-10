0bf56ecb14d91ff6720d360f8fd62c072ccab8b8
OmurTokman <60912865+OmurTokman@users.noreply.github.com>
2022-03-10 15:06:21 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace QR_CODE_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 200);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "QR Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(425, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Add image to sw drw";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 225);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "QR Code Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode;
+using MessagingToolkit.QRCode.Codec;
+using SolidWorks.Interop;
+using SolidWorks.Interop.sldworks;
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.Diagnostics;
+using SolidWorks.Interop.swconst;
+using System.Runtime.InteropServices;
+
+
+namespace QR_CODE_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        QRCodeEncoder code = new QRCodeEncoder();
+        Image rsm;
+
+        //public SldWorks swApp;
+        public partial class SolidWorksMacro
+        {
+            IModelDoc2 swModel;
+            IFeature swFeat;
+            ISketchPicture swSketchPicture;
+            ISelectionMgr swSelMgr;
+            bool boolstatus;
+            double width;
+            double height;
+            double angle;
+            public void Main()
+            {
+                swModel = swApp.IActiveDoc2;
+                swSelMgr = swModel.ISelectionManager;
+                //swModel.SketchManager.InsertSketch(true);
+                swSketchPicture = swModel.SketchManager.InsertSketchPicture2("C:\\Users\\Omur\\Desktop\\QR CODE-png.png", false);
+                //swModel.SketchManager.InsertSketch(true);
+            }
+             public SldWorks swApp;
+         }
+            private void Form1_Load(object sender, EventArgs e)
+            {
+            }
+            private void button1_Click(object sender, EventArgs e)
+            {
+                rsm = code.Encode(textBox1.Text);
+                pictureBox1.Image = rsm;
+            }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();//yeni bir kaydetme diyaloğu oluşturuyoruz.
+
+                sfd.Filter = "PNG (*.png) | *.png";
+                //    "jpeg (*.jpg)|*.jpg   |Bitmap(*.bmp)|*.bmp";    //.bmp veya .jpg olarak kayıt imkanı sağlıyoruz.
+                //sfd.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG" +
+                //"|All files(*.*)|*.*";
+                sfd.Title = "Kayıt";//diğaloğumuzun başlığını belirliyoruz.
+
+                sfd.FileName = "QR CODE";//kaydedilen resmimizin adını 'resim' olarak belirliyoruz.
+
+                DialogResult sonuç = sfd.ShowDialog();
+
+                if (sonuç == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(sfd.FileName);//Böylelikle resmi istediğimiz yere kaydediyoruz.
+                }
+            }
+
+            private void button3_Click(object sender, EventArgs e)
+            {
+
+            SolidWorksMacro swclass = new SolidWorksMacro();
+            swclass.Main();
+
+            // Call Main
+
+        }
+
+    }
+    }
+
